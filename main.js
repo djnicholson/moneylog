dc00937c0e9b57aae2194868a1925e4d829cc540
David Nicholson <me@djnicholson.com>
2019-02-08 22:21:28 +0000

    nodeintegration deprecation warning

--- main.js
+++ main.js
@@ -5,27 +5,27 @@ const { app, BrowserWindow } = require('electron');
 let win;
 
 var createWindow = function() {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile('index.html');
-  win.on('closed', () => {
-    win = null
-  })
+    win = new BrowserWindow({ width: 800, height: 600, webPreferences: { nodeIntegration: true } });
+    win.loadFile('index.html');
+    win.on('closed', () => {
+        win = null;
+    });
 };
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
 });
