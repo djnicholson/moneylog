77c6bc3a3bd928f6f21c3e83f6065c69d6589f8c
David Nicholson <me@djnicholson.com>
2019-02-10 18:07:28 +0000

    show recipe creation options

--- js_renderer/_newconnection.js
+++ js_renderer/_newconnection.js
@@ -1,7 +1,9 @@
 var MAX_STEP = 4;
 
 var scraperId = null;
+var url = null;
 var step = 0;
+var extractedData = null;
 
 var prepareUi = function() {
     for (var i = 0; i < MAX_STEP; i++) {
@@ -16,35 +18,78 @@ var prepareUi = function() {
             section.hide();
         }
     }
+
+    var clickables = $("#clickables");
+    var typables = $("#typables");
+    var numbers = $("#numbers");
+
+    clickables.empty();
+    typables.empty();
+    numbers.empty();
+
+    if (extractedData) {
+        extractedData.clickables.forEach(item => clickables.append(optionClickable(item)));
+        extractedData.typables.forEach(item => typables.append(optionTypable(item)));
+        extractedData.numbers.forEach(item => numbers.append(optionNumber(item)));
+    }
 };
 
-var loadScraper = function() {
+var closeScraperIfOpen = function() {
     if (scraperId != null) {
         moneylog.ipc.closeScraper(scraperId);
         scraperId = null;
+        extractedData = null;
     }
+}
 
-    var url = $("input[type=url]").val();
-    
+var loadScraper = function() {
+    closeScraperIfOpen();
+    url = $("input[type=url]").val();
     scraperId = moneylog.ipc.openScraper(url);
     step = 1;
-
     prepareUi();
-
     return false; // prevent form submission
 };
 
-var extractNumericalValues = function() {
-    if (scraperId == null) {
-        step = 0;
-    } else {
-        step = 2;
-    }
-
+var loginTestDone = function() {
+    closeScraperIfOpen();
+    step = 2;
     prepareUi();
-
     return false; // prevent form submission
 };
+
+var optionClickable = function(item) {
+    var element = $($("#template-clickable").html());
+    element.find(".-id").text(item.id);
+    element.find(".-value").text(item.value);
+    return element;
+};
+
+var optionTypable = function(item) {
+    var element = $($("#template-typable").html());
+    element.find(".-id").text(item.id);
+    element.find(".-value").text(item.value);
+    return element;
+};
+
+var optionNumber = function(item) {
+    var element = $($("#template-number").html());
+    element.find(".-id").text(item.id);
+    element.find(".-value").text(item.value);
+    return element;
+};
+
+var startRecording = function() {
+    closeScraperIfOpen();
+    scraperId = moneylog.ipc.openScraper(url);
+    step = 3;
+    prepareUi();
+    return false; // prevent form submission
+};
+
+document.body.onunload = function() {
+    closeScraperIfOpen();
+}
 
 document.body.onload = function() {
     
@@ -52,18 +97,21 @@ document.body.onload = function() {
         if (closedId === scraperId) {
             scraperId = null;
             step = 0;
+            extractedData = null;
             prepareUi();
         }
     });
 
     moneylog.ipc.on("scraper-data", function(_, data) {
         if (data.id === scraperId) {
-            console.log(data);
+            extractedData = data.data;
+            prepareUi();
         }
     });
 
     $("#urlEntry").submit(loadScraper);
-    $("#loginTest").submit(extractNumericalValues);
+    $("#loginTest").submit(loginTestDone);
+    $("#recordingIntro").submit(startRecording);
     
     prepareUi();
 };
